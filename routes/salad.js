e11ac074cbb91c6278baf673e39b645101e4aff2
Bohdan Tertyshchev <bobterya@gmail.com>
2023-04-30 13:15:20 +0300

    Populate Mongo

---
+++ routes/salad.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const SaladController = require('../controllers/salad.controller');
+const {findIngredient} = require('../middleware/getIngredient');
+const saladRouter = Router();
+
+saladRouter.post('/', findIngredient,SaladController.createSalad);
+saladRouter.get('/', SaladController.getAllSalads);
+saladRouter.get('/:saladId', SaladController.getSalad);
+saladRouter.patch('/:saladId', SaladController.updateSalad);
+saladRouter.delete('/:saladId', SaladController.deleteSalad);
+
+module.exports = saladRouter;
